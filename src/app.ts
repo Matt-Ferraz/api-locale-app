ce73e265d8f9f760acf4e385200e491bafc9f7da
Mateus Ferraz <108159617+Matt-Ferraz@users.noreply.github.com>
2024-02-07 00:49:53 -0300

    Not important, check point

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express from 'express'
+import authRoutes from './routes/user/auth'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
-app.use(express.json());
+app.use(express.json())
+app.use('/api/auth', authRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
+
--- src/routes/user/auth.ts
+++ src/routes/user/auth.ts
@@ -1,16 +1,16 @@
-import { Router } from 'express';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import pool from '@/database/db';
+import { Router } from 'express'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import pool from '../../database/db'
 
-const router = Router();
+const router = Router()
 
 router.post('/register', async (req, res) => {
-  // Implementar lógica de registro
-});
+
+})
 
 router.post('/login', async (req, res) => {
-  // Implementar lógica de login
-});
 
-export default router;
+})
+
+export default router
